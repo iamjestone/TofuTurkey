aedc38c7beb089a57d7c1ab4df0c0daa676d6b27
Joan <iamjestone@gmail.com>
2019-09-21 21:03:18 -0400

    Joans fix 1

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # D3Project
+To Initiate the Project:
+1. Open final_project_folder
+2. Open html_rm folder
+3. Open index.html file in Visual Studio Code
+4. Run python -m http.server in Terminal
+5. Open local host in browser
+
+PROJECT SUMMARY:
+- From Tofu to Turkey: Healthy for you? Healthy for the Earth?
+  - An exploration of the nutrient value of food and the resources used to produce it
+
+PROJECT CONCEPT
+- Initial Idea
+  - Growth of Vegetarian/Veganism and the “presumed” corresponding growth in sales of “meatless” products
+  - Lack of accurate data on number of vegetarians/vegans
+  - Difficulty finding robust sales data
+
+- Evolution
+  - Discovered USDA & Oxford University data for nutritional value of products and resource usage when producing key ingredients
+
+- Final Project
+  - Measuring the nutritional value, emissions, land and water usage of vegan and non-vegan products
+
+- DATA: ETL/CRUD
+  - Used USDA Food Composition Databases & University of Oxford Environmental Data
+  - Narrowed database with emissions, land, and water usage down to key categories
+  - Created list of meat-based foods (hamburger, turkey burger, etc.) and their corresponding meatless substitutes (bean burger, mushroom burger, etc.)
+  - Used list to create a query that provide nutrient values for selected foods
+
+- GRAPHS
+  - Nutritional Value of a Non-Vegan Item and its Vegan Substitute
+    - Energy (Kcal), Fat (g), Carbs (g), Fiber (g), Protein (g), & Sugar (g)
+  - Emissions & Land and Water Usage in the Production of Food Items
+    - Nutrient Units, Land Use, CO2 Emissions, SO2 Emissions (sulfur dioxide), PO4 Emissions (phosphate), Freshwater Withdrawals
+
+- CHALLENGES
+  - Accessing the API
+  - Flask/Heroku
+  - Working non-linearly
--- app.py
+++ app.py
@@ -1,101 +1,83 @@
-import os
-
-import pandas as pd
+from flask import Flask, render_template, jsonify
+from sqlalchemy import create_engine
 import numpy as np
-
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#################################################
-# Database Setup
-#################################################
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+# setup Postgres connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/EnvironmentalData'
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 
+class EnvironmentalData(db.Model):
+    __tablename__ = "NurtritionData"
+    Food_ID = db.Column(db.String, primary_key=True)
+    Name = db.Column(db.Numeric)
+    Energy_kcal = db.Column(db.Numeric)
+    Carbohydrates_g = db.Column(db.Numeric)
+    Fat_g = db.Column(db.Numeric)
+    Fiber_g = db.Column(db.Numeric)
+    Protein_g = db.Column(db.Numeric)
+    Sugar_g = db.Column(db.Numeric)
+    Type = db.Column(db.String)
+    Group = db.Column(db.String)
+
+    __tablename__ = "clean_environment_data"
+    Product = db.Column(db.String, primary_key=True)
+    NutrUnits = db.Column(db.Numeric)
+    LandUse = db.Column(db.Numeric)
+    CO2Emissions = db.Column(db.Numeric)
+    SO2Emissions = db.Column(db.Numeric)
+    PO4Emissions = db.Column(db.Numeric)
+    FreshwaterWithdrawals = db.Column(db.Numeric)
+    Type = db.Column(db.String)
+
 # reflect an existing database into a new model
-Base = automap_base()
+#Base = automap_base()
 # reflect the tables
-Base.prepare(db.engine, reflect=True)
+#Base.prepare(engine, reflect=True)
 
-# Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
-Samples = Base.classes.samples
-
+# Save reference to the table
+#NutritionData = Base.classes.NutritionData
+#clean_environment_data = Base.classes.clean_environment_data
 
 @app.route("/")
-def index():
-    """Return the homepage."""
-    return render_template("index.html")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/foodtypes<br/>"
+        f"/api/v1.0/foodproducts"
+    )
 
-
-@app.route("/names")
+@app.route("/api/v1.0/foodtypes")
 def names():
-    """Return a list of sample names."""
+    # session = Session(engine)
+    results = db.session.query(NutritionData.Group).all()
 
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+    # Convert list of tuples into normal list
+    all_groups = list(np.ravel(results))
 
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
-
-
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
-    sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
-    ]
-
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-    # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
-    for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
-
-    print(sample_metadata)
-    return jsonify(sample_metadata)
+    return jsonify(all_groups)
 
 
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+@app.route("/api/v1.0/foodproducts")
+def types():
 
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
+    #session2 = Session(engine)
+    results2 = db.session.query(clean_environment_data.Product).all()
 
+    # Convert list of tuples into normal list
+    all_groups2 = list(np.ravel(results2))
 
+    return jsonify(all_groups2)
+   
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
